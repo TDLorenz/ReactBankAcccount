96b79685ff8f9b8ac2495f0a4a80495774a3c154
TDLorenz <troydaniello.nyc@gmail.com>
2021-04-24 18:31:05 -0400

    added link to credits

--- react-router/src/components/Home.js
+++ react-router/src/components/Home.js
@@ -1,19 +1,21 @@
-   // src/components/Home.js
-import React, {Component} from 'react';
+// src/components/Home.js
+import React, { Component } from 'react';
 import AccountBalance from './AccountBalance';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
-        <div>
-          <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
-          <h1>Bank of React</h1>
-          <AccountBalance accountBalance={this.props.accountBalance}/>
-          <Link to="/userProfile">User Profile</Link>          
-          <br></br>
-          <Link to="/debits">Debits</Link>          
-        </div>
+      <div>
+        <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank" />
+        <h1>Bank of React</h1>
+        <AccountBalance accountBalance={this.props.accountBalance} />
+        <Link to="/userProfile">User Profile</Link>
+        <br></br>
+        <Link to="/debits">Debits</Link>
+        <br></br>
+        <Link to="/credits">Credits</Link>
+      </div>
     );
   }
 }
