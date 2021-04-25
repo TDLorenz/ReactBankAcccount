ec1cc402a95f3490d0ec0fa0b4dddc890b713ff9
TDLorenz <troydaniello.nyc@gmail.com>
2021-04-25 19:53:10 -0400

    inpit amount for credits and debits now rounded to 2 dec places

--- react-router/src/components/Debits.js
+++ react-router/src/components/Debits.js
@@ -1,53 +1,54 @@
 //Debits
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import AccountBalance from './AccountBalance';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Debits extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            debit: {
-                id: 0, 
-                description: '',
-                amount: 0,
-                date: new Date().toISOString()
-            },
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      debit: {
+        id: 0,
+        description: '',
+        amount: 0,
+        date: new Date().toISOString()
+      },
     }
+  }
 
-    makeDebit = (debitsData, found) => {
-        let currData = debitsData; 
-        let foundMatch = found; 
-        let table = [];
-        //found is false when we get 404 error
-        if (!foundMatch) {
-            table.push(<tr key={-1}><td>No Debits</td></tr>);
-            return table;
-        } else {
-            currData.forEach(debit => {
-                let id = debit.id
-                let description = debit.description; 
-                let amount = debit.amount
-                let date = debit.date    
-                table.push(
-                    <tr className="row" key={id}>
-                        <td className="text">Description: {description}</td>
-                        <td className="text">Amount: {amount}</td>
-                        <td className="text">Date: {date}</td>
-                    </tr>
-                );
-            });
-        }
-        return table;
+  makeDebit = (debitsData, found) => {
+    let currData = debitsData;
+    let foundMatch = found;
+    let table = [];
+    //found is false when we get 404 error
+    if (!foundMatch) {
+      table.push(<tr key={-1}><td>No Debits</td></tr>);
+      return table;
+    } else {
+      currData.forEach(debit => {
+        let id = debit.id
+        let description = debit.description;
+        let amount = Number(debit.amount).toFixed(2);
+        //let amount = debit.amount.toFixed(2);
+        let date = debit.date
+        table.push(
+          <tr className="row" key={id}>
+            <td className="text">Description: {description}</td>
+            <td className="text">Amount: {amount}</td>
+            <td className="text">Date: {date}</td>
+          </tr>
+        );
+      });
     }
+    return table;
+  }
   handleChange = (e) => {
-    const newDebit = {...this.state.debit}
+    const newDebit = { ...this.state.debit }
     const inputField = e.target.name
     const inputValue = e.target.value
     newDebit[inputField] = inputValue
-    this.setState({debit: newDebit})
+    this.setState({ debit: newDebit })
   }
   handleSubmit = (e) => {
     e.preventDefault()
@@ -55,33 +56,33 @@ class Debits extends Component {
   }
 
   render() {
-    let handleUpdate = this.props.handleUpdate; 
+    let handleUpdate = this.props.handleUpdate;
     return (
-        <div>
-          <h3> Debits </h3>  
-          User: {this.props.user.userName}
-          <AccountBalance accountBalance={this.props.accountBalance}/>
-          <Link to="/">Return to Home</Link>
-          <br></br>
-          <div className="table">
-            <table id="data">
-                <tbody>
-                    {this.makeDebit(this.props.apiData, this.props.found)}
-                </tbody>
-            </table>
-          </div>
-          <form onSubmit={this.handleSubmit}>
-            <div>
-              <label htmlFor="description">Enter a description</label>
-              <input type="text" name="description" onChange={this.handleChange} value={this.state.debit.description} />
-            </div>
-            <div>
-              <label htmlFor="amount">Enter an amount</label>
-              <input type="number" name="amount"onChange={this.handleChange} value={this.state.debit.amount}/>
-            </div>
-            <button>Add Debit</button>
-          </form>
+      <div>
+        <h3> Debits </h3>
+        User: {this.props.user.userName}
+        <AccountBalance accountBalance={this.props.accountBalance} />
+        <Link to="/">Return to Home</Link>
+        <br></br>
+        <div className="table">
+          <table id="data">
+            <tbody>
+              {this.makeDebit(this.props.apiData, this.props.found)}
+            </tbody>
+          </table>
         </div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="description">Enter a description</label>
+            <input type="text" name="description" onChange={this.handleChange} value={this.state.debit.description} />
+          </div>
+          <div>
+            <label htmlFor="amount">Enter an amount</label>
+            <input type="number" name="amount" onChange={this.handleChange} value={this.state.debit.amount} />
+          </div>
+          <button>Add Debit</button>
+        </form>
+      </div>
     );
   }
 }
