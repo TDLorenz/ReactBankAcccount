e62b86be6f8ee77ca55e9f67b3b74f34609eeffa
TDLorenz <troydaniello.nyc@gmail.com>
2021-04-25 20:46:40 -0400

    added some formatting to all pages

--- react-router/src/components/Login.js
+++ react-router/src/components/Login.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import "./Styles.css"
 
 class LogIn extends Component {
-  constructor () {
+  constructor() {
     super()
     this.state = {
       user: {
@@ -14,38 +16,47 @@ class LogIn extends Component {
   }
 
   handleChange = (e) => {
-    const updatedUser = {...this.state.user}
+    const updatedUser = { ...this.state.user }
     const inputField = e.target.name
     const inputValue = e.target.value
     updatedUser[inputField] = inputValue
 
-    this.setState({user: updatedUser})
+    this.setState({ user: updatedUser })
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
     this.props.mockLogIn(this.state.user)
-    this.setState({redirect: true})
+    this.setState({ redirect: true })
   }
 
-  render () {
+  render() {
     if (this.state.redirect) {
-      return (<Redirect to="/userProfile"/>)
+      return (<Redirect to="/userProfile" />)
     }
 
     return (
-      <div>
+      <div className="page">
+        <br />
+        <br />
+        <h1>User Profile</h1>
         <form onSubmit={this.handleSubmit}>
           <div>
             <label htmlFor="userName">User Name</label>
+            &ensp;
             <input type="text" name="userName" onChange={this.handleChange} value={this.state.user.userName} />
           </div>
+          <br />
           <div>
             <label htmlFor="password">Password</label>
+            &ensp;
             <input type="password" name="password" />
           </div>
+          <br />
           <button>Log In</button>
         </form>
+        <br />
+        <Link to="/">Return to Home</Link>
       </div>
     )
   }
--- react-router/src/components/UserProfile.js
+++ react-router/src/components/UserProfile.js
@@ -1,17 +1,19 @@
 // src/components/UserProfile.js
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Styles.css'
 
 class UserProfile extends Component {
   render() {
     return (
-        <div>
-          <h1>User Profile</h1>
-
-          <div>Username: {this.props.userName}</div>
-          <div>Member Since: {this.props.memberSince}</div>
-          <Link to="/">Return to Home</Link>
-        </div>
+      <div className="page">
+        <h1>User Profile</h1>
+        <div>Username: {this.props.userName}</div>
+        <br />
+        <div>Member Since: {this.props.memberSince}</div>
+        <br />
+        <Link to="/">Return to Home</Link>
+      </div>
     );
   }
 }
